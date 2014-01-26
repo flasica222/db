42557a2da0d166f3deacedf4f72f15b09e65e264
Flavius Ignat <flavius.ignat2@gmail.com>
2014-01-26 21:25:42 +0200

    Some tests for \MySQL\Connection.php and \MySQL\Table.php

--- tests/Jasny/DB/MySQL/TableTest.php
+++ tests/Jasny/DB/MySQL/TableTest.php
@@ -2,12 +2,12 @@
 
 /**
  * Tests for Jasny\DB\MySQL\Table.
- * 
+ *
  * The MySQL user needs to have full permissions for `dbtest`.*.
- * 
+ *
  * Please configure default mysqli settings in your php.ini.
  * Alternatively run as `php -d mysqli.default_user=USER -d mysqli.default_pw=PASSWORD /usr/bin/phpunit`
- * 
+ *
  * @author Arnold Daniels
  */
 /** */
@@ -18,7 +18,7 @@ require_once __DIR__ . '/TestCase.php';
 
 /**
  * Tests for MySQL\Table.
- * 
+ *
  * @package Test
  * @subpackage MySQL
  */
@@ -33,7 +33,7 @@ class TableTest extends TestCase
         parent::setUp();
         $this->table = Table::factory('foo', $this->db);
     }
-    
+
     /**
      * Test Table::getDefaultConnection
      */
@@ -41,15 +41,15 @@ class TableTest extends TestCase
     {
         $this->assertSame($this->db, Table::getDefaultConnection());
         $this->assertSame($this->db, \Jasny\DB\Table::getDefaultConnection());
-        
+
         $newdb = $this->getMockBuilder('Jasny\DB\MySQL\Connection')->disableOriginalConstructor()->getMock();
         \Jasny\DB\Table::$defaultConnection = $newdb;
-        
+
         $this->assertSame($this->db, Table::getDefaultConnection());
         $this->assertSame($newdb, \Jasny\DB\Table::getDefaultConnection());
     }
-    
-    
+
+
     /**
      * Test Table::factory
      */
@@ -59,21 +59,21 @@ class TableTest extends TestCase
         $this->assertInstanceOf('Jasny\DB\MySQL\Table', $foo);
         $this->assertSame($this->db, $foo->db());
         $this->assertSame('foo', $foo->getTableName());
-        
+
         $foo_bar = Table::factory('foo_bar', $this->db);
         $this->assertInstanceOf('FooBarTable', $foo_bar);
         $this->assertSame('foo_bar', $foo_bar->getTableName());
 
         $this->assertSame($foo, $this->db->table('foo'));
     }
-    
+
     /**
      * Test Table::factory with a model namespace
      */
     public function testFactory_Ns()
     {
         $this->db->setModelNamespace('Test');
-        
+
         $table = Table::factory('foo', $this->db);
         $this->assertInstanceOf('Test\FooTable', $table);
         $this->assertSame('foo', $table->getTableName());
@@ -88,7 +88,7 @@ class TableTest extends TestCase
         $this->assertInstanceOf('Jasny\DB\MySQL\Table', $foo);
         $this->assertSame($this->db, $foo->db());
     }
-    
+
     /**
      * Test Table::factory with class names
      */
@@ -98,7 +98,7 @@ class TableTest extends TestCase
         $this->assertInstanceOf('Jasny\DB\MySQL\Table', $foo);
         $this->assertSame($this->db, $foo->db());
         $this->assertSame('foo', $foo->getTableName());
-        
+
         $foo_bar = Table::factory('FooBar', $this->db);
         $this->assertInstanceOf('FooBarTable', $foo_bar);
         $this->assertSame('foo_bar', $foo_bar->getTableName());
@@ -110,14 +110,14 @@ class TableTest extends TestCase
     public function testFactory_ClassNS()
     {
         $this->db->setModelNamespace('Test');
-        
+
         $foo = Table::factory('Test\Foo', $this->db);
         $this->assertInstanceOf('Jasny\DB\MySQL\Table', $foo);
         $this->assertSame($this->db, $foo->db());
         $this->assertSame('foo', $foo->getTableName());
     }
 
-    
+
     /**
      * Test Table::getRecordClass
      */
@@ -129,18 +129,18 @@ class TableTest extends TestCase
         $foo_bar = Table::factory('foo_bar', $this->db);
         $this->assertSame('Jasny\DB\Record', $foo_bar->getRecordClass());
     }
-    
+
     /**
      * Test Table::getRecordClass with a model namespace
      */
     public function testGetClass_Ns()
     {
         $this->db->setModelNamespace('Test');
-        
+
         $table = Table::factory('foo', $this->db);
         $this->assertSame('Test\Foo', $table->getRecordClass());
     }
-    
+
     /**
      * Test Table::getDefaults
      */
@@ -164,7 +164,7 @@ class TableTest extends TestCase
     {
         $this->assertSame('id', $this->table->getPrimarykey());
     }
-    
+
     /**
      * Test Table::getPrimarykey with combined primary keys
      */
@@ -180,20 +180,20 @@ class TableTest extends TestCase
     {
         $records = $this->table->fetchAll();
         $this->assertEquals(5, count($records));
-        
+
         $this->assertInstanceOf('Foo', $records[0]);
         $this->assertSame($this->table, $records[0]->getDBTable());
-        
+
         $this->assertEquals(1, $records[0]->id);
         $this->assertEquals('Foo', $records[0]->name);
         $this->assertEquals('tv', $records[0]->ext);
-        
+
         $this->assertEquals(2, $records[1]->id);
         $this->assertEquals(3, $records[2]->id);
         $this->assertEquals(4, $records[3]->id);
         $this->assertEquals(5, $records[4]->id);
     }
-    
+
     /**
      * Test Table::fetch
      */
@@ -203,14 +203,14 @@ class TableTest extends TestCase
 
         $this->assertInstanceOf('Foo', $record);
         $this->assertSame($this->table, $record->getDBTable());
-        
+
         $this->assertEquals(3, $record->id);
         $this->assertEquals('Zoo', $record->name);
         $this->assertEquals('tv', $record->ext);
-        
+
         $this->assertNull($this->table->fetch(99));
     }
-    
+
     /**
      * Test Table::fetch with a filter
      */
@@ -220,11 +220,11 @@ class TableTest extends TestCase
 
         $this->assertInstanceOf('Foo', $record);
         $this->assertSame($this->table, $record->getDBTable());
-        
+
         $this->assertEquals(3, $record->id);
         $this->assertEquals('Zoo', $record->name);
         $this->assertEquals('tv', $record->ext);
-        
+
         $this->assertNull($this->table->fetch(array('name'=>'No')));
     }
 
@@ -237,28 +237,62 @@ class TableTest extends TestCase
 
         $this->assertInstanceOf('Foo', $record);
         $this->assertSame($this->table, $record->getDBTable());
-        
+
         $this->assertEquals(3, $record->id);
         $this->assertEquals('Zoo', $record->name);
         $this->assertEquals('tv', $record->ext);
-        
+
         $this->assertNull($this->table->fetch(array('name'=>'No')));
     }
-    
+
+    /**
+     * Test Table::fetchValue()
+     */
+    public function testFetchValue()
+    {
+        $record = $this->table->fetchValue("name",2);
+        $this->assertEquals("Bar",$record);
+    }
+
+    /**
+     * Test Table::fetchValue() argument $id not used in the DB
+     */
+    public function testFetchValue_nonExistentRecord()
+    {
+        $record = $this->table->fetchValue("name",99999);
+        $this->assertEquals(null, $record);
+    }
+
+    /**
+     * Test Table::fetchValue() argument $field not found in DB
+     */
+    public function testFetchValue_nonExistentField()
+    {
+        try
+        {
+            $this->table->fetchValue("nonExistent",1);
+            $this->fail("No MySQL\Exception was thrown");
+        }
+        catch(\Exception $e)
+        {
+            $this->assertEquals("Unknown column 'nonExistent' in 'field list'", $e->getError());
+        }
+    }
+
     /**
      * Test Table::save
      */
     public function testSave()
     {
         self::$reuse_db = false;
-        
+
         $record = $this->table->fetch(1);
         $record->name = 'KLM';
         $id = $this->table->save($record);
 
         $this->assertSame(1, $id);
         $this->assertEquals(1, $record->id);
-        
+
         $result = $this->db->query("SELECT * FROM foo WHERE id = 1");
         $this->assertEquals(array('id'=>1, 'name'=>'KLM', 'ext'=>'tv'), $result->fetch_assoc());
     }
@@ -269,14 +303,14 @@ class TableTest extends TestCase
     public function testSave_Values()
     {
         self::$reuse_db = false;
-        
+
         $id = $this->table->save(array('id'=>1, 'name'=>'KLM'));
         $this->assertEquals(1, $id);
 
         $result = $this->db->query("SELECT * FROM foo WHERE id = 1");
         $this->assertEquals(array('id'=>1, 'name'=>'KLM', 'ext'=>'tv'), $result->fetch_assoc());
     }
-    
+
     /**
      * Test Table::save with a new record
      */
@@ -284,19 +318,19 @@ class TableTest extends TestCase
     {
         return;
         self::$reuse_db = false;
-        
+
         $record = new \Foo();
         $record->name = 'MAN';
         $record->ext = 'mu';
         $ret = $this->table->save($record);
-        
+
         $this->assertSame($record, $ret);
         $this->assertEquals(6, $record->id);
-        
+
         $result = $this->db->query("SELECT * FROM foo WHERE id = 6");
         $this->assertEquals(array('id'=>6, 'name'=>'MAN', 'ext'=>'mu'), $result->fetch_assoc());
     }
-    
+
     /**
      * Test Table::save with a new record
      */
@@ -304,16 +338,16 @@ class TableTest extends TestCase
     {
         return;
         self::$reuse_db = false;
-        
+
         $record = $this->table->save(array('name'=>'MAN', 'ext'=>'mu'));
         $this->assertEquals(6, $record->id);
         $this->assertEquals('MAN', $record->name);
         $this->assertEquals('mu', $record->ext);
-        
+
         $result = $this->db->query("SELECT * FROM foo WHERE id = 6");
         $this->assertEquals(array('id'=>6, 'name'=>'MAN', 'ext'=>'mu'), $result->fetch_assoc());
     }
-    
+
     /**
      * Test Table::save to a table without an auto_increment field
      */
@@ -321,4 +355,9 @@ class TableTest extends TestCase
     {
         $this->markTestIncomplete();
     }
+//
+//    public function testCount()
+//    {
+//        echo(count(100));
+//    }
 }
